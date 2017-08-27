7c64141c0311b91135ae343a8ff145248126a40b
Shaliko Usubov <shaliko@ezid.ru>
2017-08-27 23:59:18 +0300

    Add Echo cordova plugin

--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -23,6 +23,13 @@
     [self.commandDelegate runInBackground:^{
         NSString* payload = nil;
         // Some blocking logic...
+        for (int i = 1; i <= 1000000000; i++)
+        {
+            int r = arc4random_uniform(i);
+            if(i % 100000000 == 0) {
+                NSLog(@"%d", i);
+            }
+        }
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
         // The sendPluginResult method is thread-safe.
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
